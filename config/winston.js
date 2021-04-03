52dd0dd7bcf7a0258d179414747bab94da55d497
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-04-03 09:49:22 -0400

    Added log configs

---
+++ config/winston.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const {
+  createLogger,
+  format,
+  transports,
+} = require('winston');
+
+const {
+  combine,
+  timestamp,
+  colorize,
+  simple,
+} = format;
+
+const logPath = (filename) => path.resolve(__dirname, '..', 'logs', filename);
+const logger = createLogger({
+  level: 'info',
+  defaultMeta: { service: 'winston-logging-example' },
+  transports: [
+    new transports.Console({
+      format: combine(
+        colorize(),
+        timestamp(),
+        simple(),
+      ),
+    }),
+    new transports.File({
+      level: 'error',
+      filename: logPath('error.log'),
+      format: combine(
+        timestamp(),
+        simple(),
+      ),
+    }),
+    new transports.File({
+      level: 'info',
+      filename: logPath('combined.log'),
+      format: combine(
+        timestamp(),
+        simple(),
+      ),
+    }),
+  ],
+});
+
+module.exports = logger;
