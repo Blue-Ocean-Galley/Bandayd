41bbf6cea46bb5d2e79d3880192a8b4df7c91749
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-03 11:38:30 -0400

    add button prset

--- client/src/styles/globalStyles.js
+++ client/src/styles/globalStyles.js
@@ -1,11 +1,33 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    border-radius: 5px;
+  }
+  html {
+    font-size: 62.5%;
+  }
   body {
     background: ${({ theme }) => theme.primary};
     color: ${({ theme }) => theme.primary_text};
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+    font-size: 160%;
+  }
+`;
+
+const ButtonPreset = styled.button`
+  text-transform: uppercase;
+  margin: 0;
+  padding: 1rem;
+  background-color: ${({ theme }) => theme.primary_400};
+  border: none;
+  color: ${({ theme }) => theme.primary_text};
+  cursor: pointer;
+  &:hover {
+    background-color: ${({ theme }) => theme.primary_600};
+    color: ${({ theme }) => theme.secondaryText};
   }
 `;
 
 export default GlobalStyle;
+export const Button = ButtonPreset;
