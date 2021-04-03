8adb8f3383aa962be43476dd2236697bfbf6e65d
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-03 11:05:52 -0400

    Fix export

--- client/src/styles/globalStyles.js
+++ client/src/styles/globalStyles.js
@@ -1,11 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyles = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.primary};
     color: ${({ theme }) => theme.primary_text};
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
   }
-  `;
+`;
 
-export default GlobalStyles;
+export default GlobalStyle;
