579db520071bee52bc0e56feff2c27378c6a3b62
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-03 15:56:42 -0400

    Renders landing page -- change to functional component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles, { Button, Card, Tile } from './styles/globalStyles';
+import GlobalStyles from './styles/globalStyles';
 import darkTheme from './styles/theme';
+import SplashPageContainer from './splashPage/splashPageContainer';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isEslintWorking: true,
-    };
-  }
-
-  render() {
-    const { isEslintWorking } = this.state;
-    return (
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyles />
-        <main className="container" id="app-outer-div">
-          <h1 id="title" className="header-text">Bandayd</h1>
-          <div>
-            { isEslintWorking ? 'Hello world!' : '' }
-          </div>
-          <Button>?</Button>
-          <Tile>
-            <Card>
-              <div> A card </div>
-            </Card>
-            <Card>
-              <div> A card </div>
-            </Card>
-          </Tile>
-        </main>
-      </ThemeProvider>
-    );
-  }
+export default function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles />
+      <main className="container" id="app-outer-div">
+        <h1 id="title" className="header-text">Bandayd</h1>
+        <SplashPageContainer />
+      </main>
+    </ThemeProvider>
+  );
 }
-
-export default App;
