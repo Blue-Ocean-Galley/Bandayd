454ac80c623ed4065080af49a77c74ee9821c224
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-05 15:01:15 -0400

    comment out plan

--- client/src/editBlogPage/EditBlogContainer.jsx
+++ client/src/editBlogPage/EditBlogContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import {
   Card,
@@ -8,6 +8,18 @@ import {
 } from '../styles/globalStyles';
 
 export default function EditBlogPageContainer() {
+  // pass in band name/id
+  // get all blog posts for that band
+  // for each, create a  card
+  // list: date/title of blog post
+  // on card click, edit mode:
+  // populate edit tile with that post's text
+  // display save/cancel buttons
+
+  // if save button is clicked, put request with updated text
+  // if cancel is clicked, end edit mode, clear tile, hide buttons
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <>
       <Tile>
@@ -18,12 +30,14 @@ export default function EditBlogPageContainer() {
       <PastBlogPosts>
         <Card>
           Past Blog Posts
-          <Button> Create New </Button>
+          <Button onClick={() => setIsEditing(true)}> Create New </Button>
         </Card>
       </PastBlogPosts>
       <Tile>
         <Card>
           Blog Post
+          <Button onClick={() => setIsEditing(false)}> Cancel </Button>
+          <Button onClick={() => setIsEditing(false)}> Save </Button>
         </Card>
       </Tile>
     </>
