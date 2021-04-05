6484a35e31d85434de9a078bdf72211f484b5a56
abachant <aubreybachant@gmail.com>
2021-04-05 11:37:51 -0400

    Create basic react router for <App />

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/globalStyles';
 import darkTheme from './styles/theme';
@@ -6,12 +12,18 @@ import SplashPageContainer from './splashPage/splashPageContainer';
 
 export default function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyles />
-      <main>
-        <h1>Bandayd</h1>
-        <SplashPageContainer />
-      </main>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyles />
+      </ThemeProvider>
+      <Switch>
+        <Route exact path="/">
+          <main>
+            <h1>Bandayd</h1>
+            <SplashPageContainer />
+          </main>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
