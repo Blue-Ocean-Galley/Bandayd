82018859491292a7e99c85fee04bbb2317b37136
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-04-06 17:46:59 -0400

    Added color overlay prop to image tiles

--- client/src/imageTiles/ImageTile.jsx
+++ client/src/imageTiles/ImageTile.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ImageTile = ({ imgUrl }) => (
+const ImageTile = ({ imgUrl, overlay }) => (
   <>
-    <ImgWrapper imgUrl={imgUrl} />
+    <ImgWrapper imgUrl={imgUrl} overlay={overlay} />
   </>
 );
 
+const Transparency = styled.div`
+max-width: 48rem;
+height: 30rem;
+background: rgba(39,62,84,0.82);
+z-index: 2;
+
+`;
 const ImgWrapper = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   flex-grow: 1;
   margin: 1px;
   max-width: 48rem;
   height: 30rem;
-  background-image: url("${props => props.imgUrl}");
+  background: linear-gradient(to right, ${props => props.overlay}, ${props => props.overlay}), url("${props => props.imgUrl}") no-repeat top center;
+  z-index: 1;
 
   &:hover {
     box-shadow: 0 10px 20px rgba(0,0,0,0.2), 0 12px 20px rgba(0,0,0,0.23);
