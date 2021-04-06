779688e27055f79278ea88e5013b48c4dadf881d
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-04-06 17:13:52 -0400

    Added animation on hover to image tiles

--- client/src/imageTiles/ImageTile.jsx
+++ client/src/imageTiles/ImageTile.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Tile } from '../styles/globalStyles';
 
 const ImageTile = () => (
   <>
-    <ImgPlaceholder></ImgPlaceholder>
+    <ImgPlaceholder />
   </>
 );
 
@@ -15,6 +14,11 @@ const ImgPlaceholder = styled.div`
   max-width: 48rem;
   height: 30rem;
   background: yellow;
+
+  &:hover {
+    box-shadow: 0 10px 20px rgba(0,0,0,0.2), 0 12px 20px rgba(0,0,0,0.23);
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+    transform: scale(1.05) rotate(-2deg);
 `;
 
 export default ImageTile;
