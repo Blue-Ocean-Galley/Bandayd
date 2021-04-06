f766401279c16848b6796d055af3c86f2726dfcb
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-04-06 17:14:05 -0400

    Added Image block to splash page

--- client/src/splashPage/splashPageContainer.jsx
+++ client/src/splashPage/splashPageContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Hero from './HeroImage';
 import FeatureCards from './FeatureCards';
+import ImageTiles from '../imageTiles/ImageTiles';
 
 export default function SplashPageContainer({ toPath }) {
   return (
