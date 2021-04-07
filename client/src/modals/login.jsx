d4ea5d61091d856a1c9503b3d41be64b3fcf7d57
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-07 15:27:35 -0400

    Add optional callback for login to change nav button to dropdown

--- client/src/modals/login.jsx
+++ client/src/modals/login.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Modal from 'react-modal';
+import PropTypes from 'prop-types';
 
 import { Button, Tile, Form } from '../styles/globalStyles';
 
 Modal.setAppElement(document.getElementById('app'));
 Modal.defaultStyles.overlay.backgroundColor = 'rgba(0,0,0,0.6)';
 
-export default function Login() {
+export default function Login({ cb = () => {} }) {
   const [showModal, toggleModal] = useState(false);
 
   function submitLogin(e) {
@@ -36,7 +37,13 @@ export default function Login() {
 
   return (
     <>
-      <Button onClick={() => toggleModal(!showModal)}>Login</Button>
+      <Button onClick={() => {
+        toggleModal(!showModal);
+        cb();
+      }}
+      >
+        Login
+      </Button>
       <Modal
         isOpen={showModal}
         style={customStyles}
@@ -68,6 +75,9 @@ export default function Login() {
     </>
   );
 }
+Login.propTypes = {
+  cb: PropTypes.func.isRequired,
+};
 
 const LoginTile = styled(Tile)`
   display: flex;
