597ab2dbe9e8ef46175b4e8a180c5b08301cdd35
Sung <skim6502@gmail.com>
2021-04-07 16:01:25 -0400

    Finished creating routes for the blog section

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
-const { getABlog, getOther, getAllBlog } = requrie('../controllers/blog.js');
+const { getABlog, getAllBlog, updateBlog, addNewBlog } = requrie('../controllers/blog.js');
 
 const router = express.Router();
-
+//  getting all the blogs
 router.get('/blogs', getAllBlog);
 
+//  getting a single blog
 router.get('/blogs', getABlog);
 
-router.get('/other', getOther);
+//  posting a new blog
+router.post('/blogs', addNewBlog);
+
+//  updating an existing blog
+router.put('/blogs', updateBlog);
 
 module.exports = router;
