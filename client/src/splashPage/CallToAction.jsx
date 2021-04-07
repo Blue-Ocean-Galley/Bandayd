edc56f8efab4bf10e869d1d37e0ee01d86424b4a
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-07 16:29:34 -0400

    Fixed styling

--- client/src/splashPage/CallToAction.jsx
+++ client/src/splashPage/CallToAction.jsx
@@ -1,38 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom'; //not needed with modal
 import { Button, Tile } from '../styles/globalStyles';
 import Login from '../modals/login';
 
-export default function CallToAction({ toPath }) {
+export default function CallToAction() {
   return (
     <Container>
       <p>
         Ready to get  started?
       </p>
-      <Link to={toPath}>
+      <div>
         <Button> Sign Up </Button>
         <Login />
-      </Link>
+      </div>
     </Container>
   );
 }
 
-CallToAction.propTypes = {
-  toPath: PropTypes.string.isRequired,
-};
+// CallToAction.propTypes = {  //removed since log in is a modal
+//   toPath: PropTypes.string.isRequired,
+// };
 
 const Container = styled(Tile)`
   min-height: 30rem;
   background-color: ${({ theme }) => theme.primary};
   justify-content: space-around;
   align-items: center;
-  > a {
+  > div {
     padding: 2rem;
-    width: max-content;
+    width: 25rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   }
   > p {
     width: max-content;
+    font-size: 3rem;
   }
 `;
