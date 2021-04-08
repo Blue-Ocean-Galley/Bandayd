318726002920b50d80fa2a017aa701bfad167e92
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-07 23:03:02 -0400

    removed router

--- client/src/components/artistBandPage/BandPageSongList.jsx
+++ client/src/components/artistBandPage/BandPageSongList.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import BandSongListItem from './BandPageSongListItem.jsx'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from 'react-router-dom';
-
+import styled from 'styled-components';
+import { Tile } from '../../styles/globalStyles';
+import BandSongListItem from './BandPageSongListItem';
 
 class BandSongList extends React.Component {
   constructor(props) {
@@ -20,42 +15,37 @@ class BandSongList extends React.Component {
   }
 
   addSong() {
-
   }
 
   handleClickSongs(event) {
-
   }
 
   handleClickVideos(event) {
-
   }
 
   render() {
     console.log('Songs', this.props.songs)
     return (
-      <Router>
-        <div id="outer-song-list-container">
-          <div>
-            <span>Songs</span>
-            <span>Videos</span>
-          </div>
-          <div id="song-list-container">
-            <h3>Featured Songs</h3>
-            <button type="button" onClick={this.addSong.bind(this)}>+</button>
-            {this.props.songs.map((song) => {
-              return (
-                <BandSongListItem
-                  artist={song.artist}
-                  song={song.song}
-                  key={song.id}
-                />
-              )
-
-            })}
-          </div>
+      <Tile>
+        <div>
+          <span>Songs</span>
+          <span>Videos</span>
         </div>
-      </Router>
+        <div id="song-list-container">
+          <h3>Featured Songs</h3>
+          <button type="button" onClick={this.addSong.bind(this)}>+</button>
+          {this.props.songs.map((song) => {
+            return (
+              <BandSongListItem
+                artist={song.artist}
+                song={song.song}
+                key={song.id}
+              />
+            )
+
+          })}
+        </div>
+      </Tile>
     )
   }
 }
