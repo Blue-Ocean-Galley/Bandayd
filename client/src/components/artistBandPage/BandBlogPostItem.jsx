9d8d4e26ff9c24f2f528fdc8b4ba25ceae255ca1
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-08 19:54:44 -0400

    Styling

--- client/src/components/artistBandPage/BandBlogPostItem.jsx
+++ client/src/components/artistBandPage/BandBlogPostItem.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import {
   Card,
 } from '../../styles/globalStyles';
 
-export default function BandBlogPostItem({ name, post }) {
+export default function BandBlogPostItem({ name }) {
   return (
     <Post>
-      <p>{name}</p>
-      <p>{post}</p>
+      <PostName>{name}</PostName>
     </Post>
   );
 }
+BandBlogPostItem.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 const Post = styled(Card)`
-
+  justify-content: flex-start;
+  align-items: left;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  flex-direction: column;
+`;
+const PostName = styled.p`
+  font-weight: 600;
+  margin-left: 0.5rem;
+  margin-right: 2rem;
 `;
