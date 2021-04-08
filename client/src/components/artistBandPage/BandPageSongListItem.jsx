29344c72e533cc7d2efb12a7389e8df683bcc615
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-08 17:11:00 -0400

    Fix eslint errors

--- client/src/components/artistBandPage/BandPageSongListItem.jsx
+++ client/src/components/artistBandPage/BandPageSongListItem.jsx
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 export default function BandSongListItem({ trackId }) {
   return (
     // eslint-disable-next-line jsx-a11y/media-has-caption
-    <iframe src={`https://open.spotify.com/embed/track/${trackId}`} width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    // eslint-disable-next-line jsx-a11y/iframe-has-title
+    <iframe src={`https://open.spotify.com/embed/track/${trackId}`} width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media" />
   );
 }
 BandSongListItem.propTypes = {
