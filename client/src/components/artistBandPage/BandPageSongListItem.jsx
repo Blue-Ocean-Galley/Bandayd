70c456d5c2af14cd549ffd5ef42c3fe91b01fc01
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-07 23:03:33 -0400

    Converted to functional component bc its stateless

--- client/src/components/artistBandPage/BandPageSongListItem.jsx
+++ client/src/components/artistBandPage/BandPageSongListItem.jsx
@@ -1,43 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from 'react-router-dom';
 
-
-class BandSongListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  componentDidMount() {
-  }
-
-  handleChange(event) {
-
-  }
-
-  playSong() {
-
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <button type="button" onClick={this.playSong.bind(this)}>Play</button>
-          <span id="song-list-songname">{this.props.song}</span>
-          <span id="song-list-artist">{this.props.artist}</span>
-        </div>
-      </Router>
-    )
-  }
+export default function BandSongListItem({ song, artist }) {
+  return (
+    <div>
+      <button type="button">Play</button>
+      <span>{this.props.song}</span>
+      <span>{this.props.artist}</span>
+    </div>
+  );
 }
-
-export default BandSongListItem;
-
