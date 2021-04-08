8b2a1480840b06dd000b625acf3c9a3f274801e2
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-08 19:50:03 -0400

    Custom styling for media player

--- client/src/components/artistBandPage/BandPageSongListItem.jsx
+++ client/src/components/artistBandPage/BandPageSongListItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 export default function BandSongListItem({ trackId }) {
   return (
     // eslint-disable-next-line jsx-a11y/media-has-caption
     // eslint-disable-next-line jsx-a11y/iframe-has-title
-    <iframe src={`https://open.spotify.com/embed/track/${trackId}`} width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media" />
+    <Frame src={`https://open.spotify.com/embed/track/${trackId}`} width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media" />
   );
 }
 BandSongListItem.propTypes = {
@@ -14,3 +15,6 @@ BandSongListItem.propTypes = {
 BandSongListItem.defaultProps = {
   trackId: '6iwOCZFBlw3Xjj0fuO3sI6',
 };
+const Frame = styled.iframe`
+  margin: 0.5rem;
+`;
