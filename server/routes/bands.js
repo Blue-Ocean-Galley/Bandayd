97a22aa348d249c45e92f24c3c887d9bb77f8a48
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-04-09 11:26:41 -0400

    Added query to routes

--- server/routes/bands.js
+++ server/routes/bands.js
@@ -1,12 +1,14 @@
 const express = require('express');
 // const logger = require('../../config/winston');
-const { getBands, getOneBand } = require('../controllers/bands');
+const { getBands, getOneBand, editBandBio } = require('../controllers/bands');
 
 const router = express.Router();
 
 // takes page and count as query params - defaults to page=1 count=5
 router.get('/', getBands);
 // return bandId as req.params
-router.get('/:id', getOneBand);
+router.route('/:id')
+  .get(getOneBand)
+  .put(editBandBio);
 
 module.exports = router;
