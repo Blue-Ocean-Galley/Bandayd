01cce16f2b27b335c67b1e5e5bd43efa1b900d62
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-09 15:10:02 -0400

    Add profile picture modal, position button over current picture;

--- client/src/components/artistBandPage/BandPageHeader.jsx
+++ client/src/components/artistBandPage/BandPageHeader.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Tile } from '../../styles/globalStyles';
+import AddProfilePicture from '../../modals/addProfilePicture';
 
 export default function BandPageHeader({ profileUrl }) {
   return (
     <HeaderContainer>
-      <ProfilePic src={profileUrl} alt="band-profile-pic" />
+      <ProfilePicContainer>
+        <ProfilePic src={profileUrl} alt="band-profile-pic" />
+        <AddProfilePicture />
+      </ProfilePicContainer>
       <h1> Band Name </h1>
     </HeaderContainer>
   );
@@ -21,6 +25,10 @@ const ProfilePic = styled.img`
   width: 20rem;
   height: 20rem;
   margin: 2rem;
+`;
+const ProfilePicContainer = styled.div`
+  position: relative;
+  width: fit-content;
 `;
 BandPageHeader.propTypes = {
   profileUrl: PropTypes.string,
