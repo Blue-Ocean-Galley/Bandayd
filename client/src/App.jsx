4b573e5fabc44f571040eb4b47813049735ec2da
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-04-09 17:15:16 -0400

    Ignoring the default imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,10 +8,10 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/globalStyles';
 import darkTheme from './styles/theme';
 import Navigation from './navigation/NavBar';
-import SplashPageContainer from './splashPage/SplashPageContainer';
-import EditBlogPageContainer from './editBlogPage/EditBlogPageContainer';
+//import SplashPageContainer from './splashPage/SplashPageContainer';
+//import EditBlogPageContainer from './editBlogPage/EditBlogPageContainer';
 import TourList from './tours/TourList';
-import BandPage from './components/artistBandPage/BandPage';
+// import BandPage from './components/artistBandPage/BandPage';
 // import EditBlogPageContainer from './editBlogPage/EditBlogPageContainer';
 //import BandPage from './components/artistBandPage/BandPage';
 const SplashPage = lazy(() => import('./splashPage/SplashPageContainer'));
@@ -43,11 +43,9 @@ export default function App() {
             </Suspense>
           </Route>
           <Route path="/shows">
-              
             <Suspense fallback={renderLoader()}>
               <TourList />
             </Suspense>
-
           </Route>
           <Route path="/login">
             <Suspense fallback={renderLoader()}>
