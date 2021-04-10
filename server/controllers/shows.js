e7308d3e6bbee442221cfe35d1424e0289ec114e
abachant <aubreybachant@gmail.com>
2021-04-09 21:21:07 -0400

    Add route for adding shows

--- server/controllers/shows.js
+++ server/controllers/shows.js
@@ -1,4 +1,5 @@
 const { Show } = require('../models');
+
 const logger = require('../../config/winston');
 
 exports.getShows = (req, res, next) => Show.findAll({
@@ -15,7 +16,7 @@ exports.getShows = (req, res, next) => Show.findAll({
   });
 
 exports.getBandShows = (req, res, next) => {
-  const bandID = req.params.bandID;
+  const { bandID } = req.params;
   return Show.findAll({
     attributes: { exclude: ['BandId', 'createdAt', 'updatedAt'] },
     where: {
@@ -33,87 +34,25 @@ exports.getBandShows = (req, res, next) => {
     });
 };
 
-// //  getting all the blog post
-
-// exports.getAllBlog = (req, res, next) => {
-//   const bandID = req.params.bandId;
-//   return Blog.findAll({
-//     attributes: { exclude: ['BandId', 'createdAt', 'updatedAt'] },
-//     where: {
-//       bandId: bandID,
-//     },
-//   })
-//     .then((result) => {
-//       res.send(result);
-//       next();
-//     })
-//     .catch((err) => {
-//       logger.error(err);
-//       res.send(500);
-//       next(err);
-//     });
-// };
-// //  getting a single blog post
-
-// exports.getABlog = (req, res, next) => {
-//   const blogID = req.params.id;
-//   return Blog.findAll({
-//     attributes: { exclude: ['BandId', 'createdAt', 'updatedAt'] },
-//     where: {
-//       id: blogID,
-//     },
-//   })
-//     .then((result) => {
-//       res.send(result);
-//       next();
-//     })
-//     .catch((err) => {
-//       logger.error(err);
-//       res.send(500);
-//       next(err);
-//     });
-// };
-
-// // updating the blog post of a specific user
-
-// exports.updateBlog = (req, res, next) => {
-//   const postID = req.params.id;
-//   // const updatePost = req.body.post;
-//   return Blog.update({
-//     name: req.body.name,
-//     description: req.body.description,
-//     post: req.body.post,
-//     bandId: req.body.bandId,
-//   }, {
-//     where: {
-//       id: postID,
-//     },
-//   })
-//     .then(() => {
-//       res.status(201).send('Successfully Updated');
-//       next();
-//     })
-//     .catch((err) => {
-//       logger.error(err);
-//       res.send(400);
-//       next(err);
-//     });
-// };
-
-// // add a new blog post from the specific user
-
-// exports.addNewBlog = (req, res, next) => Blog.create({
-//   name: req.body.name,
-//   description: req.body.description,
-//   post: req.body.post,
-//   bandId: req.body.bandId,
-// })
-//   .then((result) => {
-//     res.status(201).send(result);
-//     next();
-//   })
-//   .catch((err) => {
-//     logger.error(err);
-//     res.send(400);
-//     next(err);
-//   });
+// Add a new Show for a band
+exports.addShow = (req, res, next) => Show.create({
+  name: req.body.name,
+  bandName: req.body.bandName,
+  displayName: req.body.displayName,
+  showDate: req.body.showDate,
+  latitude: req.body.latitude,
+  longitude: req.body.longitude,
+  city: req.body.city,
+  state: req.body.state,
+  country: req.body.country,
+  bandId: req.body.bandID,
+})
+  .then((result) => {
+    res.status(201).send(result);
+    next();
+  })
+  .catch((err) => {
+    logger.error(err);
+    res.send(400);
+    next(err);
+  });
--- server/routes/shows.js
+++ server/routes/shows.js
@@ -1,12 +1,23 @@
 const express = require('express');
 // const logger = require('../../config/winston');
+const bodyParser = require('body-parser')
+
 const { getShows, getBandShows, addShow } = require('../controllers/shows');
 
 const router = express.Router();
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+router.use(bodyParser.json());
 
 // takes page and count as query parameters
 router.get('/', getShows);
+
 // takes bandID as parameter and returns a list of shows for that band
 router.get('/:bandID', getBandShows);
+
+// takes bandID as a parameter and inserts a new show with info from the body
+router.post('/', addShow);
 
 module.exports = router;
